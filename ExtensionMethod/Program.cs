25231f3e9438f9aa868a6a4c66fb707e32ae35ef
ricmtz <ricmtz15@gmail.com>
2019-03-21 11:01:37 -0600

    Add project files.

---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String inputStr;
+            Console.WriteLine("Write a sentense:");
+            inputStr = Console.ReadLine();
+            if (inputStr.ValidateAlphabeticString(null)) {
+                Console.WriteLine("'{0}' is a valid Alphabetic string", inputStr);
+            } else {
+                Console.WriteLine("'{0}' is not a valid Alphabetic string", inputStr);
+            }
+            if (inputStr.ValidateAlphanumericString(null)) {
+                Console.WriteLine("'{0}' is a valid Alphanumeric string", inputStr);
+            } else {
+                Console.WriteLine("'{0}' is not a valid Alphanumeric string", inputStr);
+            }
+            if (inputStr.ValidateNumericString(null)) {
+                Console.WriteLine("'{0}' is a valid Numeric string", inputStr);
+            } else {
+                Console.WriteLine("'{0}' is not a valid Numeric string", inputStr);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ ExtensionMethod/Validator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    static class Validator
+    {
+        private static bool ValidateString(this string str, char[] specialChars, Func<char, bool> fn, bool acceptSpace = true)
+        {
+            bool result = false;
+            List<char> tempStr = str.ToList();
+
+            if (acceptSpace)
+            {
+                tempStr.RemoveAll(c => c == ' ');
+            }
+            if (specialChars != null)
+            {
+                tempStr.RemoveAll(c => specialChars.Contains(c));
+            }
+
+            result = tempStr.All(c => fn(c));
+
+            return result;
+        }
+
+        public static bool ValidateAlphabeticString(this string str, char[] specialChars, bool acceptSpace = true)
+        {
+            return str.ValidateString(specialChars, char.IsLetter, acceptSpace);
+        }
+
+        public static bool ValidateAlphanumericString(this string str, char[] specialChars, bool acceptSpace = true)
+        {
+            return str.ValidateString(specialChars, char.IsLetterOrDigit, acceptSpace);
+        }
+
+        public static bool ValidateNumericString(this string str, char[] specialChars, bool acceptSpace = true)
+        {
+            return str.ValidateString(specialChars, char.IsDigit, acceptSpace);
+        }
+    }
+}
